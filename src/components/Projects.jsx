c2e8a23224985e5cd1ceef1346ded43d1fef1b0a
sunilkarki2018 <sunilkarki2018@gmail.com>
2024-01-30 11:43:51 +0200

    Height adjusted

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,7 +23,7 @@ export default function Projects() {
               href={project.link}
               key={project.image}
               className="sm:w-1/2 w-100 p-4">
-              <div className="flex relative">
+              <div className="flex relative h-full">
                 <img
                   alt="gallery"
                   className="absolute inset-0 w-full h-full object-cover object-center"
