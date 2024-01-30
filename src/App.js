4eb5e3452ca68c1f7be716452c12b96e8c7a1dfb
sunilkarki2018 <sunilkarki2018@gmail.com>
2024-01-30 15:34:35 +0200

    Contact Information added

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import NavBar from "./components/Navbar";
 import About from "./components/About";
 import Projects from "./components/Projects";
 import Skills from "./components/Skills";
+import Contact from "./components/Contact";
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <About />
       <Projects />
       <Skills />
+      <Contact />
     </main>
   );
 }
